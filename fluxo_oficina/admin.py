916537de1a1370eddf55f830e7b6e2dc39855392
Kaique Beraguas <ksakx@outlook.com>
2026-04-14 02:19:21 -0300

    Feat: clientes

--- fluxo_oficina/admin.py
+++ fluxo_oficina/admin.py
@@ -1,1 +1,7 @@
 from django.contrib import admin
+from .models import Cliente
+
+@admin.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'cpf', 'contato')
+    search_fields = ('nome', 'cpf')
--- fluxo_oficina/models.py
+++ fluxo_oficina/models.py
@@ -1,1 +1,13 @@
 from django.db import models
+
+class Cliente(models.Model):
+    nome = models.CharField(max_length=100)
+    cpf = models.CharField(max_length=14, unique=True, verbose_name="CPF")
+    contato = models.CharField(max_length=20, verbose_name="Contato (Telefone/Email)")
+
+    class Meta:
+        verbose_name = "Cliente"
+        verbose_name_plural = "Clientes"
+
+    def __str__(self):
+        return f"{self.nome} ({self.cpf})"
