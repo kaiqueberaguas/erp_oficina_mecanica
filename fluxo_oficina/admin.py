2956fed5123b0ee480f6e12cb8175b8a3a8c590c
PatriciaHokama <patricia.hokama12@gmail.com>
2026-04-21 15:57:45 -0300

    Model carro criado e registro no admin

--- fluxo_oficina/admin.py
+++ fluxo_oficina/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
-from .models import Cliente
+from .models import Cliente, Carro
 
 @admin.register(Cliente)
 class ClienteAdmin(admin.ModelAdmin):
     list_display = ('nome', 'cpf', 'contato')
     search_fields = ('nome', 'cpf')
+
+@admin.register(Carro)
+class CarroAdmin(admin.ModelAdmin):
+    list_display = ('marca', 'modelo', 'placa', 'cliente')
+    search_fields = ('marca', 'modelo', 'placa')
+    list_filter = ('marca', 'modelo', 'cliente')
+
--- fluxo_oficina/models.py
+++ fluxo_oficina/models.py
@@ -11,3 +11,16 @@ class Cliente(models.Model):
 
     def __str__(self):
         return f"{self.nome} ({self.cpf})"
+    
+class Carro(models.Model):
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE, related_name='carros')
+    marca = models.CharField(max_length=50)
+    modelo = models.CharField(max_length=50)
+    placa = models.CharField(max_length=8, unique=True, verbose_name="Placa")
+
+    class Meta:
+        verbose_name = "Carro"
+        verbose_name_plural = "Carros"
+
+    def __str__(self):
+        return f"{self.marca} {self.modelo} ({self.placa}) - {self.cliente.nome}"
